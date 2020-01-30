a8c40decb8b6e707d64e89d81d43e4fb5bf6e4bc
Petrix23 <32833558+Petrix23@users.noreply.github.com>
2020-01-30 22:33:56 +0100

    error on try outside race

---
+++ SpeedingAdventure/Assets/Scripts/GamePlay/ErrorMsg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorMsg : MonoBehaviour
+{
+    public GameObject ErrorText;
+
+    public void OnTriggerEnter()
+    {
+        StartCoroutine(ShowHideError());
+    }
+
+    IEnumerator ShowHideError()
+    {
+        ErrorText.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        ErrorText.SetActive(false);
+    }
+}
